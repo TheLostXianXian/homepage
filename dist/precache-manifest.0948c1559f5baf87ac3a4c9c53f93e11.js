322d32aeec1744b20c3d816ed2486dfdefddbad5
TheLostXianXian <lovelyxiaod@126.com>
2019-11-06 23:57:06 +0800

    add dist diretory

---
+++ dist/precache-manifest.0948c1559f5baf87ac3a4c9c53f93e11.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "723f1fb6f027199af47e",
+    "url": "/js/chunk-vendors.af6b50e8.js"
+  },
+  {
+    "revision": "1320e270000a854cbcac",
+    "url": "/js/app.8363e42e.js"
+  },
+  {
+    "revision": "c3489acd1f70179ced726c2673b69da4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1320e270000a854cbcac",
+    "url": "/css/app.b3b3e838.css"
+  }
+];
