e6a786352a241edbb25e37a74284ee77277c106c
TheLostXianXian <lovelyxiaod@126.com>
2019-11-12 10:11:13 +0800

    feat: 动态引入cdn

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,21 @@
 const isProduction = process.env.NODE_ENV === "production"
+const cdn = {
+  js: ["https://cdn.bootcss.com/vue/2.6.10/vue.min.js"]
+}
 module.exports = {
   configureWebpack: config => {
     if (isProduction) {
       config.externals = {
         vue: "Vue"
       }
+    }
+  },
+  chainWebpack: config => {
+    if (isProduction) {
+      config.plugin("html").tap(args => {
+        args[0].cdn = cdn
+        return args
+      })
     }
   }
 }
